33e00e3fe6e22b59c571d81f0af0b3de7078c459
davicotico <david.ticona.saravia@gmail.com>
2026-04-18 23:20:39 -0400

    marker with icon on OpenLayers map

--- src/pages/OpenLayersPage.vue
+++ src/pages/OpenLayersPage.vue
@@ -5,23 +5,73 @@ import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
+import { fromLonLat } from 'ol/proj';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import { Style, /*Circle as CircleStyle,*/ Icon } from 'ol/style.js';
+//import Fill from 'ol/style/Fill';
+//import Stroke from 'ol/style/Stroke';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
 
 onMounted(() => {
+  const coords = fromLonLat([-68.13446, -16.497192]);
+  const marker = new Feature({
+    geometry: new Point(coords),
+  });
+  marker.setStyle(
+    new Style({
+      image: new Icon({
+        src: '/icons/favicon-32x32.png',
+        //scale: 0.1,
+      }),
+      /*new Style({
+      image: new CircleStyle({
+        radius: 8,
+        fill: new Fill({ color: 'red' }),
+        stroke: new Stroke({ color: 'white', width: 2 }),
+      }),*/
+    }),
+  );
+  const vectorSource = new VectorSource({
+    features: [marker],
+  });
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+
   const map = new Map({
     target: 'map',
     layers: [
       new TileLayer({
         source: new OSM(),
       }),
+      vectorLayer,
     ],
     view: new View({
-      center: [0, 0],
-      zoom: 2,
+      center: coords,
+      zoom: 12,
     }),
   });
   console.log(map);
 });
 </script>
 <template>
-  <div id="map"></div>
+  <q-layout>
+    <q-page-container>
+      <q-page class="q-pa-md">
+        <div class="row q-gutter-md">
+          <div class="col-5 bg-green-2">
+            <div id="map" style="height: 400px" class="full-width"></div>
+          </div>
+          <div class="col-5">
+            <h5 class="q-my-sm">Instalar OpenLayers</h5>
+            <pre><code>npm install ol
+                </code>
+              </pre>
+          </div>
+        </div>
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
--- src/shared/components/open-layers/OpenLayers.vue
+++ src/shared/components/open-layers/OpenLayers.vue
@@ -1,4 +1,65 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import './style.css';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import { fromLonLat } from 'ol/proj';
+import { Feature } from 'ol';
+import Point from 'ol/geom/Point';
+import { Style, /*Circle as CircleStyle,*/ Icon } from 'ol/style.js';
+//import Fill from 'ol/style/Fill';
+//import Stroke from 'ol/style/Stroke';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+
+type OLAttributes = {
+  height: string;
+  coordinates: string;
+};
+//const props =
+defineProps<OLAttributes>();
+onMounted(() => {
+  const coords = fromLonLat([-68.13446, -16.497192]);
+  const marker = new Feature({
+    geometry: new Point(coords),
+  });
+  marker.setStyle(
+    new Style({
+      image: new Icon({
+        src: '/icons/favicon-16x16.png',
+        scale: 0.1,
+      }) /*new CircleStyle({
+        radius: 8,
+        fill: new Fill({ color: 'red' }),
+        stroke: new Stroke({ color: 'white', width: 2 }),
+      }),*/,
+    }),
+  );
+  const vectorSource = new VectorSource({
+    features: [marker],
+  });
+  const vectorLayer = new VectorLayer({
+    source: vectorSource,
+  });
+
+  const map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+      vectorLayer,
+    ],
+    view: new View({
+      center: coords,
+      zoom: 12,
+    }),
+  });
+  console.log(map);
+});
+</script>
 <template>
-  <div></div>
+  <div id="map" style="height: 400px" class="full-width"></div>
 </template>
