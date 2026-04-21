4850a3074cc4d8bb44e1507e524ff3cdeb29f302
davicotico <david.ticona.saravia@gmail.com>
2026-04-20 23:14:39 -0400

    add component ReadonlyField

---
+++ src/shared/components/ReadonlyField.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  label: String,
+  value: String,
+});
+</script>
+<template>
+  <q-field borderless :label="label" stack-label>
+    <template v-slot:control>
+      <div class="self-center full-width no-outline">
+        {{ value }}
+      </div>
+    </template>
+  </q-field>
+</template>
